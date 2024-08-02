9292b64f5890819c08038491e4516a0869e08492
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2024-08-02 15:11:47 +0530

    Added Mad Libs and Updated Readme

---
+++ Mad Libs/madlibs.py
@@ -0,0 +1,29 @@
+
+
+adjective1 = input("Adjective: ")
+noun1 = input("Noun: ")
+verb1 = input("Verb: ")
+noun2 = input("Noun: ")
+adverb1 = input("Adverb: ")
+verb2 = input("Verb: ")
+adjective2 = input("Adjective: ")
+animal = input("Animal: ")
+noun3 = input("Noun: ")
+adjective3 = input("Adjective: ")
+object = input("Object: ")
+place = input("Place: ")
+plural_noun = input("Plural noun: ")
+verb3 = input("Verb: ")
+noun4 = input("Noun: ")
+adjective4 = input("Adjective: ")
+adjective5 = input("Adjective: ")
+creature = input("Creature: ")
+verb4 = input("Verb: ")
+
+madlib = f"Once upon a time in a {adjective1} kingdom, there lived a {noun1} who loved to {verb1}.\
+      Every day, this {noun2} would {adverb1} {verb2} with their best friend, a {adjective2} {animal} named {noun3}.\
+          One day, they discovered a {adjective3} {object} in the {place}, which granted them three {plural_noun}.\
+              Excited, they {verb3} back to their {noun4} to plan their {adjective4} adventure.\
+                  Little did they know, a {adjective5} {creature} was watching them from the shadows, ready to {verb4} their every move."
+
+print(madlib)
